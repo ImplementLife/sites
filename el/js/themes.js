616aeb8681741c896f17545773592a4f6995af98
ImplementLife <57810945+ImplementLife@users.noreply.github.com>
2020-10-28 17:12:06 +0200

    optimized load theme (dark/ligth)

--- el/js/themes.js
+++ el/js/themes.js
@@ -1,36 +1,17 @@
-const lightTheme = "css/themes/ligth.css";
-const darkTheme = "css/themes/dark.css";
-
-const button = document.getElementById("theme-button");
-
-var currentTheme = JSON.parse(localStorage.getItem('theme'));
-
-if (currentTheme == null) { setTheme(lightTheme); } 
-else { setTheme(currentTheme); }
-
-button.addEventListener("click", function () { changeTheme(); });
-
-setTimeout(includeCSS, 600, "css/themes/color_trans.css");
-/////////////////////////////////////////////////
-
-function includeCSS(aFile, aRel) {
-    let head = window.document.getElementsByTagName('head')[0];
-    let style = window.document.createElement('link');
-    style.href = aFile;
-    style.rel = aRel || 'stylesheet';
-    head.appendChild(style);
+const button = window.document.getElementById("theme-button");
+const html = window.document.getElementsByTagName('html')[0];
+var isDark = localStorage.getItem('isDark');
+if (isDark == null) {isDark = 'false';}
+if (isDark == 'true') {button.setAttribute('checked', 'checked');}
+setTheme();
+button.addEventListener("click", function() {changeTheme();});
+function setTheme() {
+    if (isDark == 'true') {html.setAttribute('dark', '');}
+    else {html.removeAttribute('dark', '');}
+    localStorage.setItem('isDark', isDark);
 }
-
-function setTheme(theme) {
-    includeCSS(theme);
-    localStorage.setItem('theme', JSON.stringify(theme));
-    currentTheme = theme;
-    if (theme == darkTheme) {
-        button.setAttribute('checked', 'checked');
-    }
-}
-
 function changeTheme() {
-    if(currentTheme == lightTheme) { setTheme(darkTheme); } 
-    else { setTheme(lightTheme); }
+    if (isDark == 'true') {isDark = 'false';}
+    else {isDark = 'true';}
+    setTheme();
 }
